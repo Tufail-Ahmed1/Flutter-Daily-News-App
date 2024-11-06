2326886c3525621e254c16b718ef1ef084bc1505
Tufail Ahmed <128387947+Tufail-Ahmed1@users.noreply.github.com>
2024-11-06 19:46:27 +0500

    Update utilis.dart

--- lib/common/utilis.dart
+++ lib/common/utilis.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
-const apikey='2cc420de7ecd42f6aec0417588cd57a7';
+const apikey=your_api_key_here
+  //Get API From NewsData APi like this :'2cc420de7ecd42f6aec0417588cd57a7'
 const kBackgroundColor=Colors.black;
